30e7dedd3d7e0aa52cfb9ecc6e305c44c5bcf966
Baylex <71856047+Baylex@users.noreply.github.com>
2020-11-14 17:35:40 -0600

    querried the tables and created, exported a new table

--- Analysis_Projects/HP_Analysis/Queries/queries.sql
+++ Analysis_Projects/HP_Analysis/Queries/queries.sql
@@ -41,3 +41,85 @@ CREATE TABLE salaries (
 
 --Querying the database
 SELECT * FROM departments;
+
+-- Create titles table
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
+);
+
+-- Create dept_emp table 
+CREATE TABLE dept_emp (
+dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no, dept_no)
+);
+
+--Querying the database
+SELECT * FROM dept_manager;
+
+-- Delete the table
+DROP TABLE employees CASCADE;
+
+-- Directions said delete the table, but then add it back
+CREATE TABLE employees(
+	  emp_no INT NOT NULL,
+	  birth_date DATE NOT NULL,
+	  first_name VARCHAR NOT NULL,
+	  last_name VARCHAR NOT NULL,
+	  gender VARCHAR NOT NULL,
+	  hire_date DATE NOT NULL,
+	  PRIMARY KEY (emp_no)
+);
+
+-- Retirement Elligibity investigation
+-- Returned 90k names
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+-- Returned 20k names
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+-- Returned 22k names
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+-- Returned 23k names
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+-- Returned 23k names
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility, Returned 41k names
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring -- output 41380
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- creates a new data table based on the search criteria
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Run to view the new data table that was created
+SELECT * FROM retirement_info;
