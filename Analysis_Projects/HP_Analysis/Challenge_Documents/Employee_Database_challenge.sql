697ae7723aad4e50d92509f4ff30720a43426d1c
Baylex <71856047+Baylex@users.noreply.github.com>
2020-11-15 06:03:41 -0600

    two more queries

--- Analysis_Projects/HP_Analysis/Challenge_Documents/Employee_Database_challenge.sql
+++ Analysis_Projects/HP_Analysis/Challenge_Documents/Employee_Database_challenge.sql
@@ -5,6 +5,9 @@
 SELECT * FROM employees; -- Returns 300k rows
 SELECT * FROM titles; -- Returns 443k rows
 
+---------------------------------------------
+-- Deliverable 1: Retirement titles, remove duplicates
+---------------------------------------------
 -- Create a table of retirement titles with defined birthdates
 SELECT e.emp_no,
 	e.first_name,
@@ -44,6 +47,9 @@ ORDER BY COUNT(ut.title) DESC;
 -- Viewing the newly created count table
 SELECT * FROM retiring_titles; -- Returns 7 rows of titles
 
+---------------------------------------------
+-- Deliverable 2: Create Mentor List
+---------------------------------------------
 -- Create mentorship eligibility table filtering on birthdate and distinct on emp no
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	   e.first_name,
@@ -65,3 +71,46 @@ ORDER BY e.emp_no ASC, de.from_date DESC;
 
 -- Viewing the newly mentor list
 SELECT * FROM mentorship_eligibilty; -- Returns 1549 employees are eligible for mentorship
+
+---------------------------------------
+-- Deliverable 3: Write 2 additional queries
+---------------------------------------
+-- Create mentee counts per titles, is there a good match up of mentors and mentees? 
+SELECT COUNT(me.title), me.title
+INTO mentee_counts
+FROM mentorship_eligibilty AS me
+GROUP BY me.title
+ORDER BY COUNT(me.title) DESC;
+
+-- Viewing the mentee counts
+SELECT * FROM mentee_counts; -- returns 6 rows, one less than the retiring list (7 rows)
+
+-- Narrow the list of retiring employees to a list of potential mentors by 
+-- filtering on being about 35 years old when starting job with current job title
+SELECT DISTINCT ON (e.emp_no)e.emp_no, 
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO unique_mentors_list
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.from_date BETWEEN '1985-01-01' AND '1985-12-31')
+ORDER by e.emp_no ASC, t.from_date DESC;
+
+-- Check the counts that the list has been reduced
+SELECT * FROM unique_mentors_list; -- Returns 5521 rows
+
+-- Creates list of potential mentors by title counts
+SELECT COUNT(um.title), um.title
+INTO unique_mentor_count
+FROM unique_mentors_list AS um
+GROUP BY um.title
+ORDER BY COUNT(um.title) DESC;
+
+-- Returns smaller count of unique mentors by title to better match exisiting mentee criteria
+SELECT * FROM unique_mentor_count;
+
